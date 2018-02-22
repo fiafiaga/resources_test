2a1d948f621384cbbadde654b0437b70d22849d9
Belakhvostov <a.belakhvostov@ocsico.local>
2018-02-22 15:58:18 +0300

    Add project files.

---
+++ Resources_test/Models/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Resources_test.Models
+{
+    public class Model
+    {
+        [Display(Name = "Property_1", ResourceType = typeof(Resource))]
+        public string Property { get; set; }
+    }
+}
